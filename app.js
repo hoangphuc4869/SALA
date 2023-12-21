3b1481691b6d46c89608ace44a3a13ed91d038ee
hoangphuc4869 <phuchoangdo4869@gmail.com>
2023-12-21 17:13:00 +0700

    up

---
+++ app.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: "#swiper-pagination",
+  },
+});
+
+var swiper = new Swiper(".mySwiper1", {
+  navigation: {
+    nextEl: ".swiper-button-next1",
+    prevEl: ".swiper-button-prev1",
+  },
+});
